a25c9e04dd2ea55b6d2ba2436c178d566de86cdd
bikstok <jkjaerulff@gmail.com>
2024-11-29 12:15:14 +0100

    Initial Commit

---
+++ src/main/java/org/example/gruppe4_car_rental/Gruppe4CarRentalApplication.java
@@ -0,0 +1,13 @@
+package org.example.gruppe4_car_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe4CarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe4CarRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gruppe4_car_rental/Gruppe4CarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gruppe4_car_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe4CarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
